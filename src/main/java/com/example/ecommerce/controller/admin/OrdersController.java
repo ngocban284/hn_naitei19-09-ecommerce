4b547d6007e3f28ecb3f84d1c9b258da363d4e96
VietNguyen <viet7896@gmail.com>
2023-09-25 10:45:00 +0700

    feature: Add paging to order list

--- src/main/java/com/example/ecommerce/controller/admin/OrdersController.java
+++ src/main/java/com/example/ecommerce/controller/admin/OrdersController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.controller.admin;
 import com.example.ecommerce.model.Order;
 import com.example.ecommerce.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,16 @@ public class OrdersController {
     private OrderService orderService;
 
     @GetMapping("/")
-    public String getOrderList(Model model) {
-        model.addAttribute("orders", orderService.findAll());
+    public String listOrders(Model model, @RequestParam(defaultValue = "0") int page,
+                             @RequestParam(defaultValue = "7") int size) {
+        Page<Order> ordersPage = orderService.findAllPaginated(page, size, "orderDate", "desc");
+
+        model.addAttribute("ordersPage", ordersPage);
+        model.addAttribute("orders", ordersPage.getContent());
         model.addAttribute("order", new Order());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", ordersPage.getTotalPages());
+
         return "admin/orders/index";
     }
 
@@ -33,9 +41,9 @@ public class OrdersController {
     public String updateOrderStatus(@ModelAttribute("order") Order order,
                                     @RequestParam("orderId") Long id
     ) {
-
         orderService.updateOrderStatus(id, order.getStatus().getId(), order.getReason());
         return "redirect:/admin/orders/";
     }
+
 
 }
