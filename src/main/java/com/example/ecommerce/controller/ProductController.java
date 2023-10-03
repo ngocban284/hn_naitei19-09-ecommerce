baf12ed5eb8bdd5287a5c5a8992299cb1afd0ed3
mthcxg <110339956+mthcxg@users.noreply.github.com>
2023-10-03 22:21:19 +0700

    fix filter, add paging

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -26,18 +26,76 @@ public class ProductController {
 	private CategoryService categoryService;
 
 	@GetMapping("categories/{categoryId}/products")
-	public String index(@PathVariable(name = "categoryId") Integer categoryId, Model model,
-			HttpServletResponse response) throws IOException {
-		
+	public String index(@PathVariable(name = "categoryId") Integer categoryId,
+			@RequestParam(value = "page", defaultValue = "1") Integer page, Model model, HttpServletResponse response)
+			throws IOException {
+
+		int pageSize = 8;
+		Integer currentPage;
+		if (page != null) {
+			currentPage = page;
+		} else {
+			currentPage = 1;
+		}
+		List<Product> products;
+		int startIndex = (currentPage - 1) * pageSize;
 		if (categoryId == null) {
 			response.sendRedirect("/404-page");
 			String errorMessage = "Không tìm thấy sản phẩm";
 			model.addAttribute("errorMessage", errorMessage);
 			return "error";
 		}
-		List<Product> products = this.productService.getProductsByCategory(categoryId);
-		model.addAttribute("products", products);
+		products = this.productService.getProductsByCategory(categoryId);
+		int endIndex = Math.min(startIndex + pageSize, products.size());
+		List<Product> productsOnPage = products.subList(startIndex, endIndex);
+		model.addAttribute("products", productsOnPage);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("totalPages", (int) Math.ceil((double) products.size() / pageSize));
 		return "products/index";
+
+	}
+
+	@GetMapping(value = "categories/{categoryId}/products", params = { "page", "sortBy" })
+	public String sort(@PathVariable(name = "categoryId") Integer categoryId,
+			@RequestParam(value = "page", defaultValue = "1") Integer page,
+			@RequestParam(value = "sortBy") String sortBy, 
+			@RequestParam(value = "ord", required = false) String order,
+			Model model, HttpServletResponse response) throws IOException {
+
+		int pageSize = 8;
+		Integer currentPage;
+		if (page != null) {
+			currentPage = page;
+		} else {
+			currentPage = 1;
+		}
+		List<Product> products = null;
+		int startIndex = (currentPage - 1) * pageSize;
+		if (sortBy.equals("sales")) {
+			products = this.productService.sortByBuyCount(categoryId);
+			int endIndex = Math.min(startIndex + pageSize, products.size());
+			List<Product> productsOnPage = products.subList(startIndex, endIndex);
+			model.addAttribute("products", productsOnPage);
+			model.addAttribute("currentPage", currentPage);
+			model.addAttribute("totalPages", (int) Math.ceil((double) products.size() / pageSize));
+			return "products/sort/sales";
+		} else if (sortBy.equals("price")) {
+			if (order != null && !order.isEmpty()) {
+				if (order.equals("ASC")) {
+					products = this.productService.sortByPrice(categoryId, "ASC");
+				} else if (order.equals("DESC")) {
+					products = this.productService.sortByPrice(categoryId, "DESC");
+				}
+				int endIndex = Math.min(startIndex + pageSize, products.size());
+				List<Product> productsOnPage = products.subList(startIndex, endIndex);
+				model.addAttribute("products", productsOnPage);
+				model.addAttribute("currentPage", currentPage);
+				model.addAttribute("totalPages", (int) Math.ceil((double) products.size() / pageSize));
+				model.addAttribute("order", order);
+				return "products/sort/price";
+			}
+		}
+		return order;
 	}
 
 	@GetMapping("/{product_id}")
@@ -54,24 +112,103 @@ public class ProductController {
 		m.addAttribute("product", product);
 		return "products/show";
 	}
-	
+
 	@GetMapping("/search")
-	public String search(@RequestParam("category") Integer categoryId, @RequestParam("keyword") String keyword, Model model) {
-	    List<Product> products;
-	    String id =String.valueOf(categoryId);
-	    if (id.equals("-1")) {
-	        products = this.productService.searchAllCategories(keyword);
-	    } else {
-	        Category selectedCategory = this.categoryService.getReferenceById(categoryId);
-	        if (selectedCategory == null) {
-	            return "error";
-	        }
-	        products = this.productService.searchByCategory(categoryId, keyword);
-	    }
-	    
-	    model.addAttribute("products", products);
-	    return "products/index";
+	public String search(@RequestParam(value = "category", required = true) Integer categoryId,
+			@RequestParam(value = "keyword", required = true) String keyword,
+			@RequestParam(value = "page", defaultValue = "1") Integer page,
+			Model model) {
+
+		int pageSize = 8;
+		Integer currentPage;
+		if (page != null) {
+			currentPage = page;
+		} else {
+			currentPage = 1;
+		}
+		List<Product> products = null;
+		int startIndex = (currentPage - 1) * pageSize;
+		String id = String.valueOf(categoryId);
+		if (id.equals("-1")) {
+			products = this.productService.searchAllCategories(keyword);
+		} else {
+			Category selectedCategory = this.categoryService.getReferenceById(categoryId);
+			if (selectedCategory == null) {
+				return "error";
+			}
+			products = this.productService.searchByCategory(categoryId, keyword);
+		}
+
+		int endIndex = Math.min(startIndex + pageSize, products.size());
+		List<Product> productsOnPage = products.subList(startIndex, endIndex);
+		model.addAttribute("products", productsOnPage);
+		model.addAttribute("categoryId", categoryId);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("totalPages", (int) Math.ceil((double) products.size() / pageSize));
+		return "products/search/search";
 	}
-	
+
+	@GetMapping(value = "/search", params = { "category", "keyword", "page", "sortBy" })
+	public String sort(@RequestParam(value = "category", required = true) Integer categoryId,
+			@RequestParam(value = "keyword", required = true) String keyword,
+			@RequestParam(value = "page", defaultValue = "1") Integer page,
+			@RequestParam(value = "sortBy") String sortBy,
+			@RequestParam(value = "ord", required = false) String order, Model model) {
+		int pageSize = 8;
+		Integer currentPage;
+		if (page != null) {
+			currentPage = page;
+		} else {
+			currentPage = 1;
+		}
+		List<Product> products = null;
+		int startIndex = (currentPage - 1) * pageSize;
+		if (sortBy != null && !sortBy.isEmpty()) {
+			if (sortBy.equals("sales")) {
+				String id = String.valueOf(categoryId);
+				if (id.equals("-1")) {
+					products = this.productService.sortResultsByBuyCount(keyword);
+				} else {
+					products = this.productService.sortResultsByBuyCount(categoryId, keyword);
+				}
+				int endIndex = Math.min(startIndex + pageSize, products.size());
+				List<Product> productsOnPage = products.subList(startIndex, endIndex);
+				model.addAttribute("products", productsOnPage);
+				model.addAttribute("currentPage", currentPage);
+				model.addAttribute("categoryId", categoryId);
+				model.addAttribute("totalPages", (int) Math.ceil((double) products.size() / pageSize));
+				return "products/search/sortBySales";
+
+			} else if (sortBy.equals("price")) {
+				if (order != null && !order.isEmpty()) {
+					String id = String.valueOf(categoryId);
+					if (id.equals("-1")) {
+						if (order.equals("ASC")) {
+							products = this.productService.sortResultsByPrice(keyword, "ASC");
+						} else if (order.equals("DESC")) {
+							products = this.productService.sortResultsByPrice(keyword, "DESC");
+						}
+
+					} else {
+						if (order.equals("ASC")) {
+							products = this.productService.sortResultsByPrice(categoryId, keyword, "ASC");
+						} else if (order.equals("DESC")) {
+							products = this.productService.sortResultsByPrice(categoryId, keyword, "DESC");
+						}
+					}
+				}
+				int endIndex = Math.min(startIndex + pageSize, products.size());
+				List<Product> productsOnPage = products.subList(startIndex, endIndex);
+				model.addAttribute("products", productsOnPage);
+				model.addAttribute("currentPage", currentPage);
+				model.addAttribute("categoryId", categoryId);
+				model.addAttribute("totalPages", (int) Math.ceil((double) products.size() / pageSize));
+				model.addAttribute("order", order);
+				return "products/search/sortByPrice";
+			}
+		}
+		return order;
+		
+	}
 
 }
